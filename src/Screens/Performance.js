96bb4c1336c22bc329dc02d4000d0a3633ffb773
Jerry V <jvr572@gmail.com>
2020-05-01 18:09:24 -0400

    display added

--- src/Screens/Performance.js
+++ src/Screens/Performance.js
@@ -5,26 +5,57 @@ import { Layout, Text, Button } from "@ui-kitten/components";
 
 import Timer from '../components/Timer';
 import TimerInput from '../components/TimerInput';
+import TimerDisplay from "../components/TimerDisplay";
 
 
 const Perfomance = () => {
 	const [isMode, setIsMode] = useState(false);
+	const [timers, setTimers] = useState([{title:'test', time:5}]);
 
+	console.log(timers)
+
+	const showTimerInput =() => {
+		setIsMode(true);
+	};
+
+	const closeInput = () => {
+		setIsMode(false);
+	};
+
+	const AddTimerbtnHandler = timerObj => {
+		setTimers(curTimers => [
+			...curTimers, 
+			{
+				id: Math.random().toString(),
+				title: timerObj.title,
+				time: timerObj.time
+			}
+		]);
+		setIsMode(false);
+	};
+	
+	
 	return (
 		<Layout style={styles.contianer}>
 
-			<Layout>
-				<Text>Title: Time Remaining</Text>
-				<Text>How to?</Text>
-			</Layout>
-
-			<Layout>
+			<Layout style={styles.exerciseInfo}>
+				{timers.length > 0 ? 
+				<TimerDisplay 
+				title={timers[0].title}
+				time={timers[0].time}
+				/> 
+				: 
+				null}
+				{/* link to exercise clip */}<Text>How to? </Text>
 				<Text>Progress bar</Text>
 			</Layout>
-
+			
+			<Layout >
+				<TimerInput isVisible={isMode} onCancel={closeInput} addTimer={AddTimerbtnHandler}/>
+				<Button onPress={showTimerInput} >Add Timer</Button>
+			</Layout>
+			
 			<Layout>
-				<Button title='Add Timer' />
-				<TimerInput isVisible={isMode}/>
 				<Timer />
 			</Layout>
 
@@ -36,8 +67,11 @@ const styles = StyleSheet.create({
 
 	contianer: {
 		flex: 1,
-		justifyContent: 'center',
-		alignContent: 'center',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+	},
+	exerciseInfo: {
+
 	}
 })
 export default Perfomance;
--- src/components/TimerInput.js
+++ src/components/TimerInput.js
@@ -1,26 +1,67 @@
 import React, {useState} from 'react'
-import { StyleSheet, Text, View, Modal, TextInput } from 'react-native';
+import { StyleSheet, View, Modal, TextInput, Button} from 'react-native';
 
-const TimerInput = ({isVisible}) => {
-  const [time, setSeconds] = useState(null);
+const TimerInput = ({isVisible, onCancel, addTimer}) => {
+  const [time, setTime] = useState(null);
+  const [title, setTitle] = useState('');
+
+  const createTimer = () => {
+    addTimer({time, title});
+  };
+
 
   return (
-    <Modal active={isVisible}>
-      <View>
-        <TextInput 
-          placeholder = "time"
-          keyboardType = {"numeric"}
-          maxLength = {2}
-          value = {time}
-          onChangeText = {time => {
-            setSeconds(time)
-          }}
-          />
+		<Modal visible={isVisible}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.input}
+					placeholder='Exercise Name'
+					maxLength={24}
+					onChangeText={(title) => {
+						setTitle(title);
+					}}
+				/>
+
+				<TextInput
+					style={styles.input}
+					placeholder='time'
+					keyboardType={"numeric"}
+					maxLength={2}
+					value={time}
+					onChangeText={(time) => {
+						setTime(time);
+					}}
+				/>
+
+				<View style={styles.btnContainer}>
+					<Button title='Cancel' color='red' onPress={onCancel} />
+					<Button title='Add' onPress={createTimer}/>
+				</View>
+			
       </View>
-    </Modal>
-  )
+		</Modal>
+	);
 }
 
 export default TimerInput
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnContainer:{
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    width: '60%'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+})
