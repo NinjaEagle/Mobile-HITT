493d7b9f5e6e3015ea0115325e6f8277555662e9
Jerry V <jvr572@gmail.com>
2020-05-01 19:35:47 -0400

    installed progressBar package from github

--- src/Screens/Performance.js
+++ src/Screens/Performance.js
@@ -6,13 +6,15 @@ import { Layout, Text, Button } from "@ui-kitten/components";
 import Timer from '../components/Timer';
 import TimerInput from '../components/TimerInput';
 import TimerDisplay from "../components/TimerDisplay";
+import ProgressBar from "../components/ProgressBar";
 
 
 const Perfomance = () => {
 	const [isMode, setIsMode] = useState(false);
 	const [timers, setTimers] = useState([{title:'test', time:5}]);
+	const [progression, setProgression] = useState(0);
 
-	console.log(timers)
+	// console.log(timers)
 
 	const showTimerInput =() => {
 		setIsMode(true);
@@ -37,31 +39,36 @@ const Perfomance = () => {
 	
 	return (
 		<Layout style={styles.contianer}>
-
 			<Layout style={styles.exerciseInfo}>
-				{timers.length > 0 ? 
-				<TimerDisplay 
-				title={timers[0].title}
-				time={timers[0].time}
-				/> 
-				: 
-				null}
-				{/* link to exercise clip */}<Text>How to? </Text>
-				<Text>Progress bar</Text>
+				{timers.length > 0 ? (
+					<TimerDisplay title={timers[0].title} time={timers[0].time} />
+				) : null}
+				<Text>Time left:</Text>
+				<ProgressBar timeLeft={timers[0].time}/>
+				<Text>How to? </Text>
+				<Layout>
+
+					<TimerInput
+						isVisible={isMode}
+						onCancel={closeInput}
+						addTimer={AddTimerbtnHandler}
+					/>
+
+					<Button style={styles.addtimerBtn} onPress={showTimerInput}>
+						Add Timer
+					</Button>
+
+				</Layout>
 			</Layout>
-			
-			<Layout >
-				<TimerInput isVisible={isMode} onCancel={closeInput} addTimer={AddTimerbtnHandler}/>
-				<Button onPress={showTimerInput} >Add Timer</Button>
-			</Layout>
-			
+
 			<Layout>
 				<Timer />
 			</Layout>
-
 		</Layout>
 	);
 };
+
+export default Perfomance;
 
 const styles = StyleSheet.create({
 
@@ -70,8 +77,11 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-around',
 		alignItems: 'center',
 	},
+	addtimerBtn: {
+		borderRadius: 50,
+		width: '40%'
+	},
 	exerciseInfo: {
 
 	}
 })
-export default Perfomance;
